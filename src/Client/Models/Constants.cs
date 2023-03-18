c793826d603cfc8276010d1b046dfc9ea0d3fb47
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-03-18 22:47:26 +0100

    Update Constants.cs

--- src/Client/Models/Constants.cs
+++ src/Client/Models/Constants.cs
@@ -2,7 +2,7 @@ namespace Jordnaer.Client.Models;
 
 public class ErrorMessages
 {
-    public const string SomethingWentWrongTryAgain = "Noget gik galt, vent et øjeblik og prøv igen.";
-    public const string LoginFailed = "Ukendt email, eller forkert kodeord.";
-    public const string RegistrationFailed = "Det lykkedes ikke at registrere dig, vent et øjeblik og prøv igen.";
+    public const string SOMETHING_WENT_WRONG_TRY_AGAIN = "Noget gik galt, vent et øjeblik og prøv igen.";
+    public const string LOGIN_FAILED = "Ukendt email, eller forkert kodeord.";
+    public const string REGISTRATION_FAILED = "Det lykkedes ikke at registrere dig, vent et øjeblik og prøv igen.";
 }
