d4363adf7a9193922b37680d714f6695eaece82a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-03-22 21:14:16 +0100

    removed unused stuff

--- src/Server/Authorization/CurrentUser.cs
+++ src/Server/Authorization/CurrentUser.cs
@@ -10,5 +10,4 @@ public class CurrentUser
     public ClaimsPrincipal? Principal { get; set; } = default!;
 
     public string Id => Principal?.FindFirstValue(ClaimTypes.NameIdentifier)!;
-    public bool? IsAdmin => Principal?.IsInRole("admin");
 }
