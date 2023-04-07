2e6b84df659b2800e8905d0c156f54b5f7f12b09
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-04-07 09:40:55 +0200

    configure snackbar, add blazored localstorage, cleanup api baseAddress

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,7 +1,9 @@
+using Blazored.LocalStorage;
 using Jordnaer.Client;
 using Jordnaer.Client.Authentication;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,13 +11,25 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient<AuthClient>(client =>
-    client.BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}api/"));
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 builder.Services.AddHttpClient<UserClient>(client =>
-    client.BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}api/"));
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(configuration => configuration.SnackbarConfiguration = new SnackbarConfiguration
+{
+    VisibleStateDuration = 2500,
+    ShowTransitionDuration = 250,
+    BackgroundBlurred = true,
+    MaximumOpacity = 90,
+    MaxDisplayedSnackbars = 3,
+    PositionClass = Defaults.Classes.Position.TopRight,
+    HideTransitionDuration = 100,
+    ShowCloseIcon = false
+});
 
 builder.Services.AddWasmAuthentication();
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
