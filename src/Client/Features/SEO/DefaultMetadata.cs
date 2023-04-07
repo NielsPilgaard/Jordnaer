c66913f4292136b31c3f92c90ad89e32901f21a1
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-04-07 09:37:29 +0200

    add support for seo

---
+++ src/Client/Features/SEO/DefaultMetadata.cs
@@ -0,0 +1,8 @@
+namespace Jordnaer.Client.Features.SEO;
+
+public static class DefaultMetadata
+{
+    public const string TITLE = "Fællesskabet Jordnær";
+    public const string DESCRIPTION = "";
+    public const string TAGS = "trivsel, netværk, legegruppe, hjemmeskole, unschooling, mødregruppe";
+}
