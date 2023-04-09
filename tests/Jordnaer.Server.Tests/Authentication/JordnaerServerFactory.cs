2cb4a2b39d4279e34a35eb1e392b783721e13780
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-04-09 20:48:28 +0200

    use sql server container in tests

--- tests/Jordnaer.Server.Tests/Authentication/JordnaerServerFactory.cs
+++ tests/Jordnaer.Server.Tests/Authentication/JordnaerServerFactory.cs
@@ -1,45 +1,6 @@
-using System.Data.Common;
-using Jordnaer.Server.Data;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Jordnaer.Server.Tests.Authentication;
 
 public class JordnaerServerFactory : WebApplicationFactory<Program>
-{
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.ConfigureServices(services =>
-        {
-            var dbContextDescriptor = services.SingleOrDefault(
-                serviceDescriptor => serviceDescriptor.ServiceType ==
-                     typeof(DbContextOptions<JordnaerDbContext>));
-
-            services.Remove(dbContextDescriptor!);
-
-            var dbConnectionDescriptor = services.SingleOrDefault(
-                serviceDescriptor => serviceDescriptor.ServiceType ==
-                     typeof(DbConnection));
-
-            services.Remove(dbConnectionDescriptor!);
-
-            // Create open SqliteConnection so EF won't automatically close it.
-            services.AddSingleton<DbConnection>(_ =>
-            {
-                var connection = new SqliteConnection("DataSource=:memory:");
-                connection.Open();
-
-                return connection;
-            });
-
-            services.AddDbContext<JordnaerDbContext>((container, options) =>
-            {
-                var connection = container.GetRequiredService<DbConnection>();
-                options.UseSqlite(connection);
-            });
-        });
-    }
-}
+{ }
