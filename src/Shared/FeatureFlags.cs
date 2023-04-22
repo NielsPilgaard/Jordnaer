014f9d771372f0bff32079d8b163886d2d442e31
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-04-22 20:24:44 +0200

    disabled internal registration

--- src/Shared/FeatureFlags.cs
+++ src/Shared/FeatureFlags.cs
@@ -3,4 +3,5 @@ namespace Jordnaer.Shared;
 public static class FeatureFlags
 {
     public const string EVENTS = "Events";
+    public const string INTERNAL_REGISTRATION = "InternalRegistration";
 }
