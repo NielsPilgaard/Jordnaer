71ac5e7781aeef4ecd87c653f0c4590c3c9f796c
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-04-22 09:25:22 +0200

    Update JordnaerDbContext.cs

--- src/Server/Database/JordnaerDbContext.cs
+++ src/Server/Database/JordnaerDbContext.cs
@@ -6,8 +6,10 @@ namespace Jordnaer.Server.Database;
 
 public class JordnaerDbContext : IdentityDbContext<ApplicationUser>
 {
-    public DbSet<Parent> Parents { get; set; } = default!;
-    public DbSet<Child> Children { get; set; } = default!;
+    public DbSet<UserProfile> UserProfiles { get; set; } = default!;
+    public DbSet<ChildProfile> ChildProfiles { get; set; } = default!;
+    public DbSet<LookingFor> LookingFor { get; set; } = default!;
+
     //TODO
     //public DbSet<Group> Groups { get; set; } = default!;
     //public DbSet<ContactRequest> ContactRequests { get; set; } = default!;
