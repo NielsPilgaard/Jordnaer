7a4c904562fc59cdd0b1888baf06d134b6f09361
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-04-22 09:25:29 +0200

    Update DatabaseInitialyzer.cs

--- src/Server/Database/DatabaseInitialyzer.cs
+++ src/Server/Database/DatabaseInitialyzer.cs
@@ -1,3 +1,4 @@
+using Jordnaer.Shared;
 using Microsoft.EntityFrameworkCore;
 
 namespace Jordnaer.Server.Database;
@@ -11,5 +12,31 @@ public static class DatabaseInitializer
         var context = scope.ServiceProvider.GetRequiredService<JordnaerDbContext>();
 
         await context.Database.MigrateAsync();
+
+        await context.InsertLookingForDataAsync();
+    }
+
+    private static async Task InsertLookingForDataAsync(this JordnaerDbContext context)
+    {
+        if (await context.LookingFor.AnyAsync())
+        {
+            return;
+        }
+
+        context.LookingFor.AddRange(new[]
+        {
+            new LookingFor {Name = "Legeaftaler"},
+            new LookingFor {Name = "Hjemmeundervisnings-grupper"},
+            new LookingFor {Name = "Legeplads ture"},
+            new LookingFor {Name = "Mødregrupper"},
+            new LookingFor {Name = "Fædregrupper"},
+            new LookingFor {Name = "Forældre støttegrupper"},
+            new LookingFor {Name = "Sportsaktiviteter"},
+            new LookingFor {Name = "Kunst og håndværksværksaktiviteter"},
+            new LookingFor {Name = "Musik og danseaktiviteter"},
+            new LookingFor {Name = "Uddannelsesaktiviteter"}
+        });
+
+        await context.SaveChangesAsync();
     }
 }
