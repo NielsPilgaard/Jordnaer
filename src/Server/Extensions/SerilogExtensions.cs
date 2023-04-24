d7d94ef5f207b48a11b3dc60e9a5fdb0063e4b91
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-04-24 20:29:15 +0200

    add grafana loki logging

--- src/Server/Extensions/SerilogExtensions.cs
+++ src/Server/Extensions/SerilogExtensions.cs
@@ -1,6 +1,8 @@
-using Microsoft.ApplicationInsights.Extensibility;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Options;
 using Serilog;
 using Serilog.Exceptions;
+using Serilog.Sinks.Grafana.Loki;
 
 namespace Jordnaer.Server.Extensions;
 
@@ -8,7 +10,13 @@ public static class SerilogExtensions
 {
     public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder)
     {
-        builder.Host.UseSerilog((context, loggerConfiguration) =>
+        builder.Services
+            .AddOptions<GrafanaLokiOptions>()
+            .BindConfiguration("GrafanaLoki")
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        builder.Host.UseSerilog((context, provider, loggerConfiguration) =>
         {
             loggerConfiguration.ReadFrom.Configuration(context.Configuration)
                 .Enrich.WithProperty("Application", builder.Environment.ApplicationName)
@@ -17,21 +25,56 @@ public static class SerilogExtensions
 
             loggerConfiguration.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss}] [{Level}] {SourceContext}: {Message:lj}{NewLine}{Exception}");
 
-            string? applicationInsightsConnectionString = context
-                .Configuration
-                .GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING");
-
-            if (applicationInsightsConnectionString is not null)
-            {
-                loggerConfiguration.WriteTo.ApplicationInsights(
-                    new TelemetryConfiguration
-                    {
-                        ConnectionString = applicationInsightsConnectionString
-                    },
-                    TelemetryConverter.Events);
-            }
+            loggerConfiguration.WriteToLoki(provider);
         });
 
         return builder;
     }
+
+    private static void WriteToLoki(this LoggerConfiguration loggerConfiguration,
+        IServiceProvider provider)
+    {
+        var grafanaLokiOptions = provider.GetRequiredService<IOptions<GrafanaLokiOptions>>().Value;
+
+        var labels =
+            new LokiLabel[] {
+                new()
+                {
+                    Key = "environment_machine_name",
+                    Value = Environment.MachineName
+                },
+                new()
+                {
+                    Key = "environment",
+                    Value = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Not Configured"
+                }
+            };
+
+        loggerConfiguration.WriteTo.GrafanaLoki(
+            uri: grafanaLokiOptions.Uri,
+            textFormatter: new LokiJsonTextFormatter(),
+            labels: labels,
+            credentials: new LokiCredentials
+            {
+                Login = grafanaLokiOptions.Login,
+                Password = grafanaLokiOptions.Password
+            },
+            queueLimit: grafanaLokiOptions.QueueLimit);
+    }
+}
+
+
+public sealed class GrafanaLokiOptions
+{
+    [Required]
+    [Url]
+    public string Uri { get; set; } = null!;
+
+    [Required]
+    public string Login { get; set; } = null!;
+
+    [Required]
+    public string Password { get; set; } = null!;
+
+    public int QueueLimit { get; set; } = 500;
 }
