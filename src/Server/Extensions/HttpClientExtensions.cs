08ac22849c313e986b6e93fbc0d88c1b9654828a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-04-25 21:36:48 +0200

    Update HttpClientExtensions.cs

--- src/Server/Extensions/HttpClientExtensions.cs
+++ src/Server/Extensions/HttpClientExtensions.cs
@@ -10,7 +10,7 @@ public static class HttpClientExtensions
                     policyBuilder.WaitAndRetryAsync(3, retryCount => TimeSpan.FromMilliseconds(50 * retryCount)))
                 .AddTransientHttpErrorPolicy(policyBuilder =>
                     policyBuilder.CircuitBreakerAsync(
-                        handledEventsAllowedBeforeBreaking: 3,
+                        handledEventsAllowedBeforeBreaking: 5,
                         durationOfBreak: TimeSpan.FromSeconds(15)));
 }
 
