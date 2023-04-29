732442219d189d94dcccfd9af293e3ee9ef54f11
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-04-29 09:55:03 +0200

    add userprofilelookingfor and navigations

--- src/Server/Database/JordnaerDbContext.cs
+++ src/Server/Database/JordnaerDbContext.cs
@@ -9,12 +9,23 @@ public class JordnaerDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<UserProfile> UserProfiles { get; set; } = default!;
     public DbSet<ChildProfile> ChildProfiles { get; set; } = default!;
     public DbSet<LookingFor> LookingFor { get; set; } = default!;
+    public DbSet<UserProfileLookingFor> UserProfileLookingFor { get; set; } = default!;
 
     //TODO
     //public DbSet<Group> Groups { get; set; } = default!;
     //public DbSet<ContactRequest> ContactRequests { get; set; } = default!;
     //public DbSet<Notification> Notifications { get; set; } = default!;
     //public DbSet<Event> Events { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserProfile>()
+            .HasMany(e => e.LookingFor)
+            .WithMany()
+            .UsingEntity<UserProfileLookingFor>();
+
+        base.OnModelCreating(modelBuilder);
+    }
 
     public JordnaerDbContext(DbContextOptions<JordnaerDbContext> options) : base(options)
     { }
