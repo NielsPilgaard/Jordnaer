2140a9d19e91bcaf18da770a359713450f3ca68e
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-05-05 20:08:54 +0200

    load env var for database connection string first

--- src/Server/Extensions/WebApplicationBuilderExtensions.cs
+++ src/Server/Extensions/WebApplicationBuilderExtensions.cs
@@ -6,9 +6,9 @@ public static class WebApplicationBuilderExtensions
 {
     public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
     {
-
-        string dbConnectionString = builder.Configuration.GetConnectionString(nameof(JordnaerDbContext)) ??
-                                    throw new InvalidOperationException(
+        string dbConnectionString = Environment.GetEnvironmentVariable($"ConnectionStrings_{nameof(JordnaerDbContext)}")
+                                    ?? builder.Configuration.GetConnectionString(nameof(JordnaerDbContext))
+                                    ?? throw new InvalidOperationException(
                                         $"Connection string '{nameof(JordnaerDbContext)}' not found.");
         builder.Services.AddSqlServer<JordnaerDbContext>(dbConnectionString);
         builder.Services.AddHealthChecks().AddSqlServer(dbConnectionString);
