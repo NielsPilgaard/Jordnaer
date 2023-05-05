521cb0ea2641969a4ba1894834e13e750bcb7cb7
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-05-05 20:18:12 +0200

    default profile picture migration

--- src/Server/Migrations/20230505181748_DefaultProfilePicture.Designer.cs
+++ src/Server/Migrations/20230505181748_DefaultProfilePicture.Designer.cs
@@ -4,6 +4,7 @@ using Jordnaer.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jordnaer.Server.Migrations
 {
     [DbContext(typeof(JordnaerDbContext))]
-    partial class JordnaerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230505181748_DefaultProfilePicture")]
+    partial class DefaultProfilePicture
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
