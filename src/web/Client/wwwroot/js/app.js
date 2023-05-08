37679d799564ecc4aa97b14a31b9152755b765bb
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-05-08 23:11:51 +0200

    footer works!!!

--- src/web/Client/wwwroot/js/app.js
+++
@@ -1,9 +0,0 @@
-function onWindowScroll(callback) {
-    window.onscroll = function () {
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-            callback.invokeMethodAsync("SetAppBarVisibility", true);
-        } else {
-            callback.invokeMethodAsync("SetAppBarVisibility", false);
-        }
-    };
-}
