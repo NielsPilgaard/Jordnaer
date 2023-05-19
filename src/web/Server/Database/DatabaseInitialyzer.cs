e335e466be6d6ee98160b7a59fe20930f55f3cd3
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-05-19 20:55:07 +0200

    less random lookingFors

--- src/web/Server/Database/DatabaseInitialyzer.cs
+++ src/web/Server/Database/DatabaseInitialyzer.cs
@@ -14,14 +14,17 @@ public static class SeedDatabase
 
         await context.Database.MigrateAsync();
 
-        await context.InsertLookingForDataAsync();
+        var lookingFor = await context.InsertLookingForDataAsync();
 
-        await context.InsertFakeUsersAsync();
+        await context.InsertFakeUsersAsync(lookingFor);
 
         await context.SaveChangesAsync();
     }
 
-    public static async Task InsertFakeUsersAsync(this JordnaerDbContext context)
+    public static async Task InsertFakeUsersAsync(
+        this JordnaerDbContext context,
+        List<LookingFor> lookingFor,
+        int usersToGenerate = 10000)
     {
         int userProfileCount = await context.UserProfiles.CountAsync();
         if (userProfileCount > 1)
@@ -30,11 +33,6 @@ public static class SeedDatabase
         }
 
         // Danish locale is not available, nb_NO is norwegian. Close enough!
-        var lookingForFaker = new Faker<LookingFor>("nb_NO")
-            .RuleFor(lf => lf.Name, f => f.Commerce.Product())
-            .RuleFor(lf => lf.Description, f => f.Lorem.Sentence())
-            .RuleFor(lf => lf.CreatedUtc, f => f.Date.Past());
-
         var childProfileFaker = new Faker<ChildProfile>("nb_NO")
             .RuleFor(cp => cp.Id, f => Guid.NewGuid())
             .RuleFor(cp => cp.FirstName, f => f.Name.FirstName())
@@ -45,7 +43,7 @@ public static class SeedDatabase
             .RuleFor(cp => cp.PictureUrl, f => f.Internet.Avatar());
 
         var userFaker = new Faker<UserProfile>("nb_NO")
-            .RuleFor(u => u.Id, f => Guid.NewGuid().ToString())
+            .RuleFor(u => u.Id, _ => Guid.NewGuid().ToString())
             .RuleFor(u => u.UserName, f => f.Internet.UserName())
             .RuleFor(u => u.FirstName, f => f.Name.FirstName())
             .RuleFor(u => u.LastName, f => f.Name.LastName())
@@ -54,37 +52,47 @@ public static class SeedDatabase
             .RuleFor(u => u.ZipCode, f => f.Random.Int(999, 9991).ToString())
             .RuleFor(u => u.City, f => f.Address.City())
             .RuleFor(u => u.Description, f => f.Lorem.Sentence())
-            .RuleFor(u => u.LookingFor, f => lookingForFaker.Generate(f.Random.Int(1, 3)))
+            .RuleFor(u => u.LookingFor,
+                f => lookingFor
+                    .OrderBy(_ => f.Random.Int(0, lookingFor.Count))
+                    .Take(f.Random.Int(0, lookingFor.Count))
+                    .ToList())
             .RuleFor(u => u.ChildProfiles, f => childProfileFaker.Generate(f.Random.Int(1, 3)))
             .RuleFor(u => u.DateOfBirth, f => f.Date.Between(DateTime.UtcNow.AddYears(-70), DateTime.UtcNow.AddYears(-16)))
             .RuleFor(u => u.ProfilePictureUrl, f => f.Internet.Avatar())
             .FinishWith((_, userProfile) => Console.WriteLine($"UserProfile Created! UserName={userProfile.UserName}"));
 
-        var users = userFaker.Generate(10000);
+        var users = userFaker.Generate(usersToGenerate);
 
         context.AddRange(users);
     }
 
-    public static async Task InsertLookingForDataAsync(this JordnaerDbContext context)
+    public static async Task<List<LookingFor>> InsertLookingForDataAsync(this JordnaerDbContext context)
     {
         if (await context.LookingFor.AnyAsync())
         {
-            return;
+            return new List<LookingFor>();
         }
 
-        context.LookingFor.AddRange(
-            new LookingFor { Name = "Legeaftaler" },
-            new LookingFor { Name = "Legegrupper/Legestuer" },
-            new LookingFor { Name = "Hjemmepasnings-grupper" },
-            new LookingFor { Name = "Hjemmeundervisnings-grupper" },
-            new LookingFor { Name = "Privat Dagpleje" },
-            new LookingFor { Name = "Mødregrupper" },
-            new LookingFor { Name = "Fædregrupper" },
-            new LookingFor { Name = "Forældregrupper" },
-            new LookingFor { Name = "Sportsaktiviteter" },
-            new LookingFor { Name = "Kunst og håndværksværksaktiviteter" },
-            new LookingFor { Name = "Musik og danseaktiviteter" },
-            new LookingFor { Name = "Uddannelsesaktiviteter" },
-            new LookingFor { Name = "Andet" });
+        var lookingFor = new List<LookingFor>()
+        {
+            new() {Name = "Legeaftaler"},
+            new() {Name = "Legegrupper/Legestuer"},
+            new() {Name = "Hjemmepasnings-grupper"},
+            new() {Name = "Hjemmeundervisnings-grupper"},
+            new() {Name = "Privat Dagpleje"},
+            new() {Name = "Mødregrupper"},
+            new() {Name = "Fædregrupper"},
+            new() {Name = "Forældregrupper"},
+            new() {Name = "Sportsaktiviteter"},
+            new() {Name = "Kunst og håndværksværksaktiviteter"},
+            new() {Name = "Musik og danseaktiviteter"},
+            new() {Name = "Uddannelsesaktiviteter"},
+            new() {Name = "Andet"}
+        };
+
+        context.LookingFor.AddRange(lookingFor);
+
+        return lookingFor;
     }
 }
