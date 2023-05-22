ecfbfc5b938f43858b12b33108125be6c4d84c50
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-05-22 22:34:27 +0200

    fix rate limits, add search rate limit

--- src/web/Server/Extensions/RateLimitExtensions.cs
+++ src/web/Server/Extensions/RateLimitExtensions.cs
@@ -8,6 +8,7 @@ public static class RateLimitExtensions
     private const string PER_USER_RATELIMIT_POLICY = "PerUserRatelimit";
     private const string HEALTH_CHECK_RATELIMIT_POLICY = "HealthCheckRateLimit";
     private const string AUTH_RATELIMIT_POLICY = "AuthenticationRateLimit";
+    private const string USER_SEARCH_RATELIMIT_POLICY = "UserSearchRateLimit";
 
     public static IServiceCollection AddRateLimiting(this IServiceCollection services) =>
         services.AddRateLimiter(options =>
@@ -41,18 +42,26 @@ public static class RateLimitExtensions
 
             options.AddPolicy(AUTH_RATELIMIT_POLICY, _ =>
             {
-                return RateLimitPartition.GetFixedWindowLimiter(AUTH_RATELIMIT_POLICY, _ => new FixedWindowRateLimiterOptions
+                return RateLimitPartition.GetFixedWindowLimiter(context.Session.Id, _ => new FixedWindowRateLimiterOptions
                 {
                     Window = TimeSpan.FromSeconds(10),
                     AutoReplenishment = true,
                     QueueLimit = 10
                 });
             });
+
+            options.AddPolicy(USER_SEARCH_RATELIMIT_POLICY,
+                context => RateLimitPartition.GetFixedWindowLimiter(context.Session.Id, _ =>
+                    new FixedWindowRateLimiterOptions
+                    {
+                        Window = TimeSpan.FromSeconds(10),
+                        PermitLimit = 3,
+                        AutoReplenishment = true
+                    }));
         });
 
     public static IEndpointConventionBuilder RequirePerUserRateLimit(this IEndpointConventionBuilder builder) => builder.RequireRateLimiting(PER_USER_RATELIMIT_POLICY);
-
     public static IEndpointConventionBuilder RequireHealthCheckRateLimit(this IEndpointConventionBuilder builder) => builder.RequireRateLimiting(HEALTH_CHECK_RATELIMIT_POLICY);
-
-    public static IEndpointConventionBuilder RequireAuthRateLimit(this IEndpointConventionBuilder builder) => builder.RequireRateLimiting(HEALTH_CHECK_RATELIMIT_POLICY);
+    public static IEndpointConventionBuilder RequireAuthRateLimit(this IEndpointConventionBuilder builder) => builder.RequireRateLimiting(AUTH_RATELIMIT_POLICY);
+    public static IEndpointConventionBuilder RequireUserSearchRateLimit(this IEndpointConventionBuilder builder) => builder.RequireRateLimiting(USER_SEARCH_RATELIMIT_POLICY);
 }
