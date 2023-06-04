b6ba81e537a86eea3c487f399fdf5fdd9648b57e
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-06-04 22:41:50 +0200

    Update RateLimitExtensions.cs

--- src/web/Server/Extensions/RateLimitExtensions.cs
+++ src/web/Server/Extensions/RateLimitExtensions.cs
@@ -10,6 +10,8 @@ public static class RateLimitExtensions
     private const string Auth_Ratelimit_Policy = "AuthenticationRateLimit";
     private const string User_Search_Ratelimit_Policy = "UserSearchRateLimit";
 
+    private const string Anonymous_Partition = "Anonymous";
+
     public static IServiceCollection AddRateLimiting(this IServiceCollection services) =>
         services.AddRateLimiter(options =>
         {
@@ -21,7 +23,7 @@ public static class RateLimitExtensions
 
                 return RateLimitPartition.GetFixedWindowLimiter(username, _ => new FixedWindowRateLimiterOptions
                 {
-                    // 10 messages per user per 10 seconds
+                    // 50 messages per user per 15 seconds
                     Window = TimeSpan.FromSeconds(15),
                     PermitLimit = 50
                 });
@@ -39,21 +41,31 @@ public static class RateLimitExtensions
 
             options.AddPolicy(Auth_Ratelimit_Policy, context =>
             {
-                return RateLimitPartition.GetFixedWindowLimiter(context.Connection.RemoteIpAddress?.ToString() ?? context.Connection.Id, _ => new FixedWindowRateLimiterOptions
-                {
-                    // 10 messages per user per 10 seconds
-                    Window = TimeSpan.FromSeconds(10),
-                    PermitLimit = 10
-                });
+                string? username = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                return username is null
+                    ? RateLimitPartition.GetFixedWindowLimiter(Anonymous_Partition,
+                        _ => new FixedWindowRateLimiterOptions { Window = TimeSpan.FromSeconds(1), PermitLimit = 50 })
+                    : RateLimitPartition.GetNoLimiter(username);
             });
 
             options.AddPolicy(User_Search_Ratelimit_Policy,
-                context => RateLimitPartition.GetFixedWindowLimiter(context.Connection.RemoteIpAddress?.ToString() ?? context.Connection.Id, _ =>
-                    new FixedWindowRateLimiterOptions
-                    {
-                        Window = TimeSpan.FromSeconds(10),
-                        PermitLimit = 3
-                    }));
+                context =>
+                {
+                    string? username = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                    return username is not null
+                        ? RateLimitPartition.GetFixedWindowLimiter(username, _ => new FixedWindowRateLimiterOptions
+                        {
+                            Window = TimeSpan.FromSeconds(10),
+                            PermitLimit = 15
+                        })
+                        : RateLimitPartition.GetFixedWindowLimiter(Anonymous_Partition, _ => new FixedWindowRateLimiterOptions
+                        {
+                            Window = TimeSpan.FromSeconds(1),
+                            PermitLimit = 15
+                        });
+                });
         });
 
     public static IEndpointConventionBuilder RequirePerUserRateLimit(this IEndpointConventionBuilder builder) => builder.RequireRateLimiting(Per_User_Ratelimit_Policy);
