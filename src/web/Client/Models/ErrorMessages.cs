c0f093b5ebab247bc18bb874d553a1859a26026a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-06-04 22:42:27 +0200

    Update ErrorMessages.cs

--- src/web/Client/Models/ErrorMessages.cs
+++ src/web/Client/Models/ErrorMessages.cs
@@ -2,6 +2,7 @@ namespace Jordnaer.Client.Models;
 
 public class ErrorMessages
 {
+    public const string HIGH_LOAD = "Der er pt meget trafik på hjemmesiden, vent et øjeblik før du prøver igen.";
     public const string SOMETHING_WENT_WRONG_TRY_AGAIN = "Noget gik galt, vent et øjeblik og prøv igen.";
     public const string LOGIN_FAILED = "Ukendt email, eller forkert kodeord.";
     public const string REGISTRATION_FAILED = "Det lykkedes ikke at registrere dig, vent et øjeblik og prøv igen.";
