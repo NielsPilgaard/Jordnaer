65daa229e0088f1bf00eef8e3aab1db7e32ee864
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-06-14 20:12:15 +0200

    added initial sponsor page
    Hardcoded sponsors for now

---
+++ src/web/Shared/Sponsors/Sponsor.cs
@@ -0,0 +1,9 @@
+namespace Jordnaer.Shared.Sponsors;
+
+public class Sponsor
+{
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+    public required string LogoUrl { get; init; }
+    public required string Link { get; init; }
+}
