dac46fd0e9eef19f827ef4d60dbec68ebf589ec6
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-06-14 21:40:27 +0200

    more seo to more pages

--- src/web/Client/Features/SEO/DefaultMetadata.cs
+++
@@ -1,8 +0,0 @@
-namespace Jordnaer.Client.Features.SEO;
-
-public static class DefaultMetadata
-{
-    public const string TITLE = "Mini Møder";
-    public const string DESCRIPTION = "";
-    public const string TAGS = "trivsel, netværk, legegruppe, hjemmeskole, unschooling, mødregruppe";
-}
