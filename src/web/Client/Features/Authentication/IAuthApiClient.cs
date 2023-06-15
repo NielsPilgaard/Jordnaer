3db93fa599f3a710480955f46e718574f100d544
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-06-15 20:42:27 +0200

    remade authclient as refit client

---
+++ src/web/Client/Features/Authentication/IAuthApiClient.cs
@@ -0,0 +1,19 @@
+using Jordnaer.Shared;
+using Refit;
+
+namespace Jordnaer.Client.Features.Authentication;
+
+public interface IAuthApiClient
+{
+    [Get("/api/auth/current-user")]
+    Task<IApiResponse<CurrentUserDto?>> GetCurrentUserAsync();
+
+    [Post("/api/auth/login")]
+    Task<IApiResponse<bool>> LoginAsync(UserInfo userInfo);
+
+    [Post("/api/auth/register")]
+    Task<IApiResponse<bool>> CreateUserAsync(UserInfo userInfo);
+
+    [Post("/api/auth/logout")]
+    Task<IApiResponse<bool>> LogoutAsync();
+}
