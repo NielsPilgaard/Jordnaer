ea4d7055dc042747a85d8e5ab0c80f7b03b5ecc0
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-07-24 12:03:39 +0200

    Update ChatExtensions.cs

--- src/Jordnaer.Shared.Contracts/Extensions/ChatExtensions.cs
+++ src/Jordnaer.Shared.Contracts/Extensions/ChatExtensions.cs
@@ -2,11 +2,32 @@ namespace Jordnaer.Shared.Contracts.Extensions;
 
 public static class ChatExtensions
 {
-    public static string SetDefaultDisplayName(this Chat chat)
+    public static string GetDisplayName(this Chat chat, string ownUserId)
     {
-        if (chat.Recipients.TryGetNonEnumeratedCount(out var recipientCount))
+        if (chat.DisplayName is not null)
         {
-
+            return chat.DisplayName;
         }
+
+        var recipients = chat.Recipients.Where(recipient => recipient.Id != ownUserId).ToArray();
+        if (recipients.Length > 3)
+        {
+            const int recipientNamesToDisplay = 3;
+            return $"{string.Join(", ", recipients
+                .Take(recipientNamesToDisplay)
+                .Select(e => e.FirstName))} og {chat.Recipients.Count - recipientNamesToDisplay} andre";
+        }
+
+        if (recipients.Length > 1)
+        {
+            return string.Join(", ", chat.Recipients.Select(e => e.FirstName));
+        }
+
+        if (recipients.Length is 1)
+        {
+            return $"{recipients[0].FirstName} {recipients[0].LastName}";
+        }
+
+        return string.Empty;
     }
 }
