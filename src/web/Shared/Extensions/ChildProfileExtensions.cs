3f298eb803789fa4f700a39430cef10000ff5e42
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-08-08 12:50:01 +0200

    grouping extensions

---
+++ src/web/Shared/Extensions/ChildProfileExtensions.cs
@@ -0,0 +1,27 @@
+namespace Jordnaer.Shared;
+
+public static class ChildProfileExtensions
+{
+    public static void LoadValuesFrom(this ChildProfile mapInto, ChildProfile mapFrom)
+    {
+        mapInto.CreatedUtc = mapFrom.CreatedUtc;
+        mapInto.Description = mapFrom.Description;
+        mapInto.DateOfBirth = mapFrom.DateOfBirth;
+        mapInto.FirstName = mapFrom.FirstName;
+        mapInto.LastName = mapFrom.LastName;
+        mapInto.Gender = mapFrom.Gender;
+        mapInto.PictureUrl = mapFrom.PictureUrl;
+        mapInto.Id = mapFrom.Id;
+    }
+
+    public static ChildProfileDto ToChildProfileDto(this ChildProfile childProfile) =>
+        new()
+        {
+            FirstName = childProfile.FirstName,
+            LastName = childProfile.LastName,
+            Gender = childProfile.Gender,
+            PictureUrl = childProfile.PictureUrl,
+            DateOfBirth = childProfile.DateOfBirth,
+            Description = childProfile.Description
+        };
+}
---
+++ src/web/Shared/Extensions/UserProfileExtensions.cs
@@ -0,0 +1,51 @@
+namespace Jordnaer.Shared;
+
+public static class UserProfileExtensions
+{
+    public static UserProfile Map(this UserProfile dto)
+        => new()
+        {
+            Id = dto.Id,
+            UserName = dto.UserName,
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Address = dto.Address,
+            ChildProfiles = dto.ChildProfiles,
+            City = dto.City,
+            DateOfBirth = dto.DateOfBirth,
+            Description = dto.Description,
+            LookingFor = dto.LookingFor,
+            PhoneNumber = dto.PhoneNumber,
+            ZipCode = dto.ZipCode,
+            ProfilePictureUrl = dto.ProfilePictureUrl,
+            Contacts = dto.Contacts,
+            CreatedUtc = dto.CreatedUtc
+        };
+
+    public static ProfileDto ToProfileDto(this UserProfile userProfile)
+        => new()
+        {
+            FirstName = userProfile.FirstName,
+            LastName = userProfile.LastName,
+            Address = userProfile.Address,
+            ChildProfiles = userProfile.ChildProfiles
+                .Select(childProfile => childProfile.ToChildProfileDto())
+                .ToList(),
+            City = userProfile.City,
+            DateOfBirth = userProfile.DateOfBirth,
+            Description = userProfile.Description,
+            LookingFor = userProfile.LookingFor,
+            PhoneNumber = userProfile.PhoneNumber,
+            ZipCode = userProfile.ZipCode,
+            ProfilePictureUrl = userProfile.ProfilePictureUrl,
+            CreatedUtc = userProfile.CreatedUtc
+        };
+
+    public static UserSlim ToUserSlim(this UserProfile userProfile)
+        => new()
+        {
+            DisplayName = $"{userProfile!.FirstName} {userProfile.LastName}",
+            Id = userProfile.Id,
+            ProfilePictureUrl = userProfile.ProfilePictureUrl
+        };
+}
