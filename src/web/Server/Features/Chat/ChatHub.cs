28e7b3cb8407f365ba9a4da7298e1f8e716f98bf
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-08-29 21:58:27 +0200

    Update ChatHub.cs

--- src/web/Server/Features/Chat/ChatHub.cs
+++ src/web/Server/Features/Chat/ChatHub.cs
@@ -1,12 +1,21 @@
+using Jordnaer.Shared;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Jordnaer.Server.Features.Chat;
 
+[Authorize]
 public class ChatHub : Hub
 {
     public async Task SendMessage(string user, string message)
     {
         //TODO: Add azure signalR
         await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        Context.User.GetId();
+        return base.OnConnectedAsync();
     }
 }
