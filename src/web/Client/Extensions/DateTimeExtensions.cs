2aeba3c44cda1c7dbe6ec47c48350ed06a8c45ee
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-09-01 22:32:12 +0200

    implement timestamps for chat messages

--- src/web/Client/Extensions/DateTimeExtensions.cs
+++ src/web/Client/Extensions/DateTimeExtensions.cs
@@ -1,30 +1,56 @@
+using System.Globalization;
+
 namespace Jordnaer.Client.Extensions;
 
 public static class DateTimeExtensions
 {
-    public static string GetTimePassed(this DateTime date)
+    public static string DisplayExactTime(this DateTime date) =>
+        $"afsendt {date.ToLocalTime().ToString("d/M yyyy kl. HH:mm", new CultureInfo("da-DK"))}";
+
+    public static string DisplayTimePassed(this DateTime date)
     {
         var timeSpan = DateTime.UtcNow.Subtract(date);
 
-        if (timeSpan.TotalSeconds < 60)
-            return "lige sendt";
+        switch (timeSpan.TotalSeconds)
+        {
+            case < 15:
+                return "lige sendt";
+            case < 60:
+                return $"sendt for {Math.Floor(timeSpan.TotalSeconds)} sekunder siden";
+        }
 
-        if (timeSpan.TotalMinutes < 60)
-            return $"{Math.Floor(timeSpan.TotalMinutes)} minutter siden";
+        switch (timeSpan.TotalMinutes)
+        {
+            case 1:
+                return $"sendt for 1 minut siden";
+            case < 60:
+                return $"sendt for {Math.Floor(timeSpan.TotalMinutes)} minutter siden";
+        }
 
-        if (timeSpan.TotalHours < 24)
-            return $"{Math.Floor(timeSpan.TotalHours)} timer siden";
-        if (timeSpan.TotalDays < 7)
-            return $"{Math.Floor(timeSpan.TotalDays)} dage siden";
+        switch (timeSpan.TotalHours)
+        {
+            case 1:
+                return $"sendt for 1 time siden";
+            case < 24:
+                return $"sendt for {Math.Floor(timeSpan.TotalHours)} timer siden";
+        }
+
+        switch (timeSpan.TotalDays)
+        {
+            case 1:
+                return "sendt igår";
+            case < 7:
+                return $"sendt for {Math.Floor(timeSpan.TotalDays)} dage siden";
+        }
 
         int weeks = (int)Math.Floor(timeSpan.TotalDays / 7);
 
-        if (weeks is >= 1 and < 2)
-            return "sidste uge";
+        if (weeks is 1)
+            return "sendt i sidste uge";
 
         if (timeSpan.TotalDays < 365)
-            return $"{weeks} uger siden";
+            return $"sendt for {weeks} uger siden";
 
-        return $"afsendt {date.ToLocalTime():g}";
+        return $"afsendt {date.ToLocalTime():dd/MM/yyyy HH:mm:ss}";
     }
 }
