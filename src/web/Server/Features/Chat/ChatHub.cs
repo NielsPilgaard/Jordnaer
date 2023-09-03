c0bfda59d410642aae69e9b07114c0d43c9d9325
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-09-03 20:12:12 +0200

    add logging to chathub

--- src/web/Server/Features/Chat/ChatHub.cs
+++ src/web/Server/Features/Chat/ChatHub.cs
@@ -7,16 +7,43 @@ namespace Jordnaer.Server.Features.Chat;
 [Authorize]
 public class ChatHub : Hub<IChatHub>
 {
-    public async Task SendChatMessageAsync(ChatMessageDto chatMessage, string userId)
+    private readonly ILogger<ChatHub> _logger;
+
+    public ChatHub(ILogger<ChatHub> logger)
     {
-        await Clients.User(userId).ReceiveChatMessage(chatMessage);
+        _logger = logger;
     }
-    public async Task StartChatAsync(StartChat startChat)
+
+    public override async Task OnConnectedAsync()
     {
-        await Clients
+        _logger.LogDebug("User {userId} connected to {chatHub}", Context.User?.GetId(), nameof(ChatHub));
+
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        if (exception is not null)
+        {
+            _logger.LogError(exception, "User {userId} disconnected from {chatHub}. " +
+                                        "Exception message: {exceptionMessage}",
+                Context.User?.GetId(), nameof(ChatHub), exception.Message);
+        }
+        else
+        {
+            _logger.LogDebug("User {userId} disconnected from {chatHub}", Context.User?.GetId(), nameof(ChatHub));
+        }
+
+        await base.OnConnectedAsync();
+    }
+
+    public async Task SendChatMessageAsync(ChatMessageDto chatMessage, string userId)
+        => await Clients.User(userId).ReceiveChatMessage(chatMessage);
+
+    public async Task StartChatAsync(StartChat startChat)
+        => await Clients
             .Users(startChat
                 .Recipients
                 .Select(user => user.Id))
             .StartChat(startChat);
-    }
 }
