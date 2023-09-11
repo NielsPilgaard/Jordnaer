52c1d9049ef54f55632d8a2485ca758fcf5f01dc
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-09-11 20:51:30 +0200

    add summary

--- src/web/Shared/Auth/ExternalUserInfo.cs
+++ src/web/Shared/Auth/ExternalUserInfo.cs
@@ -7,6 +7,12 @@ public class ExternalUserInfo
     [Required]
     public required string Email { get; set; }
 
+    /// <summary>
+    /// This is the user id we get from the external auth providers, Facebook, Google & Microsoft.
+    /// <para>
+    /// We use it as <c>UserProfile.Id</c> and <c>ApplicationUser.Id</c>.
+    /// </para>
+    /// </summary>
     [Required]
     public required string ProviderKey { get; set; }
 }
