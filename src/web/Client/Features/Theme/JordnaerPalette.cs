79d1b647c66d4b33702a52064fc968388b77094e
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-09-26 14:08:07 +0200

    more work

--- src/web/Client/Features/Theme/JordnaerPalette.cs
+++ src/web/Client/Features/Theme/JordnaerPalette.cs
@@ -5,32 +5,32 @@ namespace Jordnaer.Client.Features.Theme;
 public static class JordnaerPalette
 {
     /// <summary>
-    /// Used as background for texts and headers
+    /// Yellow-orange. Used as background for texts and headers
     /// </summary>
     public static readonly MudColor Primary = "#dbab45";
 
     /// <summary>
-    /// Used as background for texts and headers
+    /// Green. Used as background for texts and headers
     /// </summary>
     public static readonly MudColor Secondary = "#878e64";
 
     /// <summary>
-    /// Used for body text
+    /// Dark Blue. Used for body text
     /// </summary>
     public static readonly MudColor Body = "#41556b";
 
     /// <summary>
-    /// Used for small texts, payoffs, quotes
+    /// Dark Red. Used for small texts, payoffs, quotes
     /// </summary>
     public static readonly MudColor Header = "#673417";
 
     /// <summary>
-    /// Used as background for text where <see cref="Primary"/> and <see cref="Secondary"/> are too dark/saturated.
+    /// Beige. Used as background for text where <see cref="Primary"/> and <see cref="Secondary"/> are too dark/saturated.
     /// </summary>
     public static readonly MudColor DimBackground = "#cfc1a6";
 
     /// <summary>
-    /// Rarely used as background for text where <see cref="Primary"/> and <see cref="Secondary"/> are too dark/saturated.
+    /// Pale Blue. Rarely used as background for text where <see cref="Primary"/> and <see cref="Secondary"/> are too dark/saturated.
     /// </summary>
     public static readonly MudColor RareBackground = "#a9c0cf";
 }
