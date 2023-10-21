68633e49635a6e011f2c87d4ba8d7fc2eb4ae5ed
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-10-21 20:42:28 +0200

    add todos

---
+++ tests/web/Jordnaer.UI.Tests/LoginTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace Jordnaer.UI.Tests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+[Category("UI")]
+public class LoginTests : PageTest
+{
+    [Test]
+    public void AuthenticationFlow()
+    {
+        // TODO: Run "playwright.ps1 codegen jordnaer.azurewebsites.net --save-storage=auth.json"
+
+        // TODO: Subsequent tests should use "playwright.ps1 codegen --load-storage=auth.json jordnaer.azurewebsites.net
+    }
+}
