66e84193a09544d369f1083b10d590ef076a4114
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-11-01 15:10:43 +0100

    fill out igroupclient

--- src/web/Client/Features/Groups/IGroupClient.cs
+++ src/web/Client/Features/Groups/IGroupClient.cs
@@ -1,6 +1,19 @@
+using Jordnaer.Shared;
+using Refit;
+
 namespace Jordnaer.Client.Features.Groups;
 
 public interface IGroupClient
 {
+    [Get("/api/groups/{id}")]
+    Task<IApiResponse<GroupDto>> GetGroupByIdAsync(Guid id);
 
+    [Post("/api/groups")]
+    Task<IApiResponse> CreateGroupAsync([Body] Group group);
+
+    [Put("/api/groups/{id}")]
+    Task<IApiResponse> UpdateGroupAsync(Guid id, [Body] Group group);
+
+    [Delete("/api/groups/{id}")]
+    Task<IApiResponse> DeleteGroupAsync(Guid id);
 }
