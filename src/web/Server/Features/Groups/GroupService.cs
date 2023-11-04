67dee3f6e1f9b74a54aeb57d5e7bf8fa8192c6ad
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-11-04 12:58:16 +0100

    remove todo

--- src/web/Server/Features/Groups/GroupService.cs
+++ src/web/Server/Features/Groups/GroupService.cs
@@ -15,7 +15,6 @@ public interface IGroupService
     Task<Results<NoContent, UnauthorizedHttpResult, NotFound>> DeleteGroupAsync(Guid id);
 }
 
-//TODO: Register in Program.cs
 public class GroupService : IGroupService
 {
     private readonly JordnaerDbContext _context;
