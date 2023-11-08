e6bcf4792cd22f88e29baed8074c3b9b90f8a260
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-11-08 19:28:47 +0100

    add 2 load tests

--- tests/web/Jordnaer.Load.Tests/Program.cs
+++ tests/web/Jordnaer.Load.Tests/Program.cs
@@ -1,44 +1,9 @@
 // See https://aka.ms/new-console-template for more information
 
+using Jordnaer.Load.Tests;
 using NBomber.CSharp;
-using NBomber.Http.CSharp;
-using Xunit;
 
-SimpleHttpExample();
-
-static void SimpleHttpExample()
-{
-    using var httpClient = new HttpClient();
-
-    var scenario = Scenario.Create("http_scenario", async _ =>
-        {
-            var request =
-                Http.CreateRequest("GET", "https://jordnaer.azurewebsites.net/")
-                    .WithHeader("Content-Type", "application/json");
-
-            var response = await Http.Send(httpClient, request);
-
-            return response;
-        })
-        .WithWarmUpDuration(TimeSpan.FromSeconds(3))
-        .WithLoadSimulations(
-            Simulation.RampingInject(rate: 50, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromMinutes(1)),
-            Simulation.Inject(rate: 50, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromMinutes(1)),
-            Simulation.RampingInject(rate: 0, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromMinutes(1))
-        );
-
-    var result = NBomberRunner
-        .RegisterScenarios(scenario)
-        .Run();
-
-    Assert.True(result.AllBytes > 0);
-    Assert.True(result.AllRequestCount > 0);
-    Assert.True(result.AllOkCount > 0);
-    Assert.True(result.AllFailCount == 0);
-
-    var scnStats = result.GetScenarioStats("http_scenario");
-
-    Assert.True(scnStats.Ok.Latency.MinMs > 0);
-    Assert.True(scnStats.Ok.Latency.MaxMs < 1_000);
-    Assert.True(scnStats.Fail.Request.Count == 0);
-}
+NBomberRunner.RegisterScenarios(
+    LandingPageLoadTest.MainScenario(),
+    UserSearchLoadTests.MainScenario())
+    .Run();
