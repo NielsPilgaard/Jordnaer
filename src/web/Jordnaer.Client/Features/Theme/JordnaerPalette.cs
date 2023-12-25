0f7d56211e404dd2c612b381e21e69a740c04770
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-12-25 22:15:18 +0100

    rename palette colors

--- src/web/Jordnaer.Client/Features/Theme/JordnaerPalette.cs
+++ src/web/Jordnaer.Client/Features/Theme/JordnaerPalette.cs
@@ -7,30 +7,30 @@ public static class JordnaerPalette
     /// <summary>
     /// Yellow-orange. Used as background for texts and headers
     /// </summary>
-    public static readonly MudColor Primary = "#dbab45";
+    public static readonly MudColor YellowBackground = "#dbab45";
 
     /// <summary>
     /// Green. Used as background for texts and headers
     /// </summary>
-    public static readonly MudColor Secondary = "#878e64";
+    public static readonly MudColor GreenBackground = "#878e64";
 
     /// <summary>
     /// Dark Blue. Used for body text
     /// </summary>
-    public static readonly MudColor Body = "#41556b";
+    public static readonly MudColor BlueBody = "#41556b";
 
     /// <summary>
     /// Dark Red. Used for small texts, payoffs, quotes
     /// </summary>
-    public static readonly MudColor Header = "#673417";
+    public static readonly MudColor RedHeader = "#673417";
 
     /// <summary>
-    /// Beige. Used as background for text where <see cref="Primary"/> and <see cref="Secondary"/> are too dark/saturated.
+    /// Beige. Used as background for text where <see cref="YellowBackground"/> and <see cref="GreenBackground"/> are too dark/saturated.
     /// </summary>
-    public static readonly MudColor DimBackground = "#dbd2c1"; // Changed from "#cfc1a6" to "#dbd2c1"
+    public static readonly MudColor BeigeBackground = "#dbd2c1"; // Changed from "#cfc1a6" to "#dbd2c1"
 
     /// <summary>
-    /// Pale Blue. Rarely used as background for text where <see cref="Primary"/> and <see cref="Secondary"/> are too dark/saturated.
+    /// Pale Blue. Rarely used as background for text where <see cref="YellowBackground"/> and <see cref="GreenBackground"/> are too dark/saturated.
     /// </summary>
-    public static readonly MudColor RareBackground = "#a9c0cf";
+    public static readonly MudColor PaleBlueBackground = "#a9c0cf";
 }
