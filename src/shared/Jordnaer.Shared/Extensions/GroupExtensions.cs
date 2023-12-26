a4b2ddd65350fa78c1672db5d07821625c6d333b
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-12-26 22:26:49 +0100

    remove unused stuff

--- src/shared/Jordnaer.Shared/Extensions/GroupExtensions.cs
+++ src/shared/Jordnaer.Shared/Extensions/GroupExtensions.cs
@@ -2,21 +2,6 @@ namespace Jordnaer.Shared;
 
 public static class GroupExtensions
 {
-    public static string DisplayLocation(this GroupDto groupDto)
-    {
-        if (groupDto.ZipCode is not null && groupDto.City is not null)
-        {
-            return $"{groupDto.ZipCode}, {groupDto.City}";
-        }
-
-        if (groupDto.ZipCode is not null)
-        {
-            return groupDto.ZipCode.ToString()!;
-        }
-
-        return groupDto.City ?? "Område ikke angivet";
-    }
-    
     public static string DisplayLocation(this GroupSlim groupDto)
     {
         if (groupDto.ZipCode is not null && groupDto.City is not null)
