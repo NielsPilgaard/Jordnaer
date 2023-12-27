6a7bbd78d80a21804aad76d789881cfa45e4835e
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-12-27 21:32:37 +0100

    Create ApiResponseExtensions.cs

---
+++ src/web/Jordnaer.Client/Extensions/ApiResponseExtensions.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using Jordnaer.Client.Models;
+using MudBlazor;
+using Refit;
+
+namespace Jordnaer.Client;
+
+public static class ApiResponseExtensions
+{
+	public static async Task<TResponse?> NotifyUserOfResponseAsync<TResponse>(
+		this Task<IApiResponse<TResponse>> apiCall,
+		ISnackbar snackbar)
+	{
+		var response = await apiCall;
+		switch (response.StatusCode)
+		{
+			case { } when response is { IsSuccessStatusCode: true, Content: not null }:
+				return response.Content!;
+
+			case HttpStatusCode.TooManyRequests:
+				snackbar.Add(ErrorMessages.High_Load, Severity.Info);
+				break;
+
+			default:
+				snackbar.Add(ErrorMessages.Something_Went_Wrong_Refresh, Severity.Warning);
+				break;
+		}
+
+		return default(TResponse?);
+	}
+}
