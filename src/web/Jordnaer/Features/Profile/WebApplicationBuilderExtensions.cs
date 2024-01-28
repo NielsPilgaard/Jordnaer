82c32646d83385c3fe6aceff363a46efbe0c3b8e
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-01-28 20:04:59 +0100

    remove profileclient

--- src/web/Jordnaer/Features/Profile/WebApplicationBuilderExtensions.cs
+++ src/web/Jordnaer/Features/Profile/WebApplicationBuilderExtensions.cs
@@ -1,12 +1,9 @@
-using Jordnaer.Extensions;
-
 namespace Jordnaer.Features.Profile;
 
 public static class WebApplicationBuilderExtensions
 {
-	public static WebApplicationBuilder AddProfileServices(this WebApplicationBuilder builder, string baseUrl)
+	public static WebApplicationBuilder AddProfileServices(this WebApplicationBuilder builder)
 	{
-		builder.Services.AddRefitClient<IProfileClient>(baseUrl);
 		builder.Services.AddScoped<IProfileCache, ProfileCache>();
 
 		return builder;
