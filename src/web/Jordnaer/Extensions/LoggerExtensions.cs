415c07dc182a891d3781a234e1efaa8590f1fa1f
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-02-18 13:03:41 +0100

    add exception logging extension

--- src/web/Jordnaer/Extensions/LoggerExtensions.cs
+++ src/web/Jordnaer/Extensions/LoggerExtensions.cs
@@ -5,4 +5,6 @@ namespace Jordnaer.Extensions;
 public static class LoggerExtensions
 {
 	public static void LogFunctionBegan(this ILogger logger, LogLevel logLevel = LogLevel.Information, [CallerMemberName] string methodName = "") => logger.Log(logLevel, "Running {method_name}", methodName);
+
+	public static void LogException(this ILogger logger, Exception exception, [CallerMemberName] string methodName = "") => logger.LogError(exception, "Exception occurred in {method_name}", methodName);
 }
