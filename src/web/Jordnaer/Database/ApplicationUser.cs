a5f1902ce920fe7ae8c3e1c8f455e430e25bb30d
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-02-19 09:17:15 +0100

    add cookie field to ApplicationUser
    + migrations

--- src/web/Jordnaer/Database/ApplicationUser.cs
+++ src/web/Jordnaer/Database/ApplicationUser.cs
@@ -1,5 +1,16 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Jordnaer.Database;
 
-public class ApplicationUser : IdentityUser { }
+public class ApplicationUser : IdentityUser
+{
+	/// <summary>
+	/// Cookies may not be larger than 4096 bytes, so StringLength 10_000 should be plenty.
+	/// <para>
+	/// <seealso href="https://chromestatus.com/feature/4946713618939904"/>
+	/// </para>
+	/// </summary>
+	[StringLength(10_000)]
+	public string? Cookie { get; set; }
+}
---
+++ src/web/Jordnaer/Migrations/20240219081648_Add_ApplicationUser_Cookie.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jordnaer.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_ApplicationUser_Cookie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cookie",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                maxLength: 10000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cookie",
+                table: "AspNetUsers");
+        }
+    }
+}
