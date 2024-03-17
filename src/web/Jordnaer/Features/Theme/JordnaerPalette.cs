fa11a4b92f6be6f2e49a711cef290348642dc17b
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-03-17 21:14:05 +0100

    removed dark mode

--- src/web/Jordnaer/Features/Theme/JordnaerPalette.cs
+++ src/web/Jordnaer/Features/Theme/JordnaerPalette.cs
@@ -1,6 +1,30 @@
+using MudBlazor;
 using MudBlazor.Utilities;
 
 namespace Jordnaer.Features.Theme;
+
+public static class JordnaerTheme
+{
+	public static readonly MudTheme CustomTheme = new()
+	{
+		Typography = new Typography
+		{
+			Body1 = new Body1
+			{
+				FontFamily = ["Open Sans Light", "Arial", "sans-serif"]
+			},
+			Body2 = new Body2
+			{
+				FontFamily = ["Open Sans Light", "Arial", "sans-serif"]
+			},
+			Default = new Default
+			{
+				FontFamily = ["Open Sans Light", "Arial", "sans-serif"],
+				FontSize = "1rem"
+			}
+		}
+	};
+}
 
 public static class JordnaerPalette
 {
