0a37d97040c3bde9397413fdc64ae0688471f290
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-03-18 20:33:15 +0100

    add elmah.io serilog sink \o/

--- src/shared/Jordnaer.Shared.Infrastructure/SerilogExtensions.cs
+++ src/shared/Jordnaer.Shared.Infrastructure/SerilogExtensions.cs
@@ -6,91 +6,96 @@ using Microsoft.Extensions.Options;
 using Serilog;
 using Serilog.Events;
 using Serilog.Exceptions;
+using Serilog.Sinks.ElmahIo;
 using Serilog.Sinks.Grafana.Loki;
 
 namespace Jordnaer.Shared.Infrastructure;
 
 public static class SerilogExtensions
 {
-    public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder)
-    {
-        builder.Services
-            .AddOptions<GrafanaLokiOptions>()
-            .BindConfiguration(GrafanaLokiOptions.SectionName)
-            .ValidateDataAnnotations()
-            .ValidateOnStart();
+	public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder)
+	{
+		builder.Services
+			.AddOptions<GrafanaLokiOptions>()
+			.BindConfiguration(GrafanaLokiOptions.SectionName)
+			.ValidateDataAnnotations()
+			.ValidateOnStart();
 
-        builder.Host.UseSerilog((context, provider, loggerConfiguration) =>
-        {
-            loggerConfiguration.ReadFrom.Configuration(context.Configuration)
-                .Enrich.WithProperty("Application", builder.Environment.ApplicationName)
-                .Enrich.FromLogContext()
-                .Enrich.WithExceptionDetails();
+		builder.Host.UseSerilog((context, provider, loggerConfiguration) =>
+		{
+			loggerConfiguration.ReadFrom.Configuration(context.Configuration)
+				.Enrich.WithProperty("Application", builder.Environment.ApplicationName)
+				.Enrich.FromLogContext()
+				.Enrich.WithExceptionDetails();
 
-            loggerConfiguration.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss}] [{Level}] {SourceContext}: {Message:lj}{NewLine}{Exception}");
+			loggerConfiguration.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss}] [{Level}] {SourceContext}: {Message:lj}{NewLine}{Exception}");
 
-            loggerConfiguration.WriteToLoki(provider);
-        });
+			loggerConfiguration.WriteToLoki(provider);
 
-        return builder;
-    }
+			loggerConfiguration.WriteTo.ElmahIo(
+				new ElmahIoSinkOptions(apiKey: "47b4edcca90b48e79f4bf487ab2f1606",
+									   logId: new Guid("d6047f4f-273c-45e1-ba5c-b3c2a8d8ac92")));
+		});
 
-    public static IApplicationBuilder UseSerilog(this WebApplication app) =>
-        app.UseSerilogRequestLogging(options => options.GetLevel = (context, _, exception) =>
-            context.Response.StatusCode switch
-            {
-                >= 500 when exception is not null => LogEventLevel.Error,
-                _ when exception is not null => LogEventLevel.Error,
-                >= 400 => LogEventLevel.Warning,
-                _ => app.Environment.IsDevelopment() ? LogEventLevel.Information : LogEventLevel.Debug
-            });
+		return builder;
+	}
 
-    private static void WriteToLoki(this LoggerConfiguration loggerConfiguration,
-        IServiceProvider provider)
-    {
-        var grafanaLokiOptions = provider.GetRequiredService<IOptions<GrafanaLokiOptions>>().Value;
+	public static IApplicationBuilder UseSerilog(this WebApplication app) =>
+		app.UseSerilogRequestLogging(options => options.GetLevel = (context, _, exception) =>
+			context.Response.StatusCode switch
+			{
+				>= 500 when exception is not null => LogEventLevel.Error,
+				_ when exception is not null => LogEventLevel.Error,
+				>= 400 => LogEventLevel.Warning,
+				_ => app.Environment.IsDevelopment() ? LogEventLevel.Information : LogEventLevel.Debug
+			});
 
-        var labels =
-            new LokiLabel[] {
-                new()
-                {
-                    Key = "environment_machine_name",
-                    Value = Environment.MachineName
-                },
-                new()
-                {
-                    Key = "environment",
-                    Value = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Not Configured"
-                }
-            };
+	private static void WriteToLoki(this LoggerConfiguration loggerConfiguration,
+		IServiceProvider provider)
+	{
+		var grafanaLokiOptions = provider.GetRequiredService<IOptions<GrafanaLokiOptions>>().Value;
 
-        loggerConfiguration.WriteTo.GrafanaLoki(
-            uri: grafanaLokiOptions.Uri,
-            textFormatter: new LokiJsonTextFormatter(),
-            labels: labels,
-            credentials: new LokiCredentials
-            {
-                Login = grafanaLokiOptions.Login,
-                Password = grafanaLokiOptions.Password
-            },
-            queueLimit: grafanaLokiOptions.QueueLimit);
-    }
+		var labels =
+			new LokiLabel[] {
+				new()
+				{
+					Key = "environment_machine_name",
+					Value = Environment.MachineName
+				},
+				new()
+				{
+					Key = "environment",
+					Value = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Not Configured"
+				}
+			};
+
+		loggerConfiguration.WriteTo.GrafanaLoki(
+			uri: grafanaLokiOptions.Uri,
+			textFormatter: new LokiJsonTextFormatter(),
+			labels: labels,
+			credentials: new LokiCredentials
+			{
+				Login = grafanaLokiOptions.Login,
+				Password = grafanaLokiOptions.Password
+			},
+			queueLimit: grafanaLokiOptions.QueueLimit);
+	}
 }
 
 
 public sealed class GrafanaLokiOptions
 {
-    public const string SectionName = "GrafanaLoki";
+	public const string SectionName = "GrafanaLoki";
 
-    [Required(ErrorMessage = "Påkrævet.")]
-    [Url]
-    public string Uri { get; set; } = null!;
+	[Required(ErrorMessage = "Påkrævet.")]
+	[Url]
+	public string Uri { get; set; } = null!;
 
-    [Required(ErrorMessage = "Påkrævet.")]
-    public string Login { get; set; } = null!;
+	[Required(ErrorMessage = "Påkrævet.")]
+	public string Login { get; set; } = null!;
 
-    [Required(ErrorMessage = "Påkrævet.")]
-    public string Password { get; set; } = null!;
+	[Required(ErrorMessage = "Påkrævet.")]
+	public string Password { get; set; } = null!;
 
-    public int QueueLimit { get; set; } = 500;
+	public int QueueLimit { get; set; } = 500;
 }
