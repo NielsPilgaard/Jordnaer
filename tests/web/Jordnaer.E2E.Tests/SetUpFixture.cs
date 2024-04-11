ed825790d60e59c7302a869996dff1e9598ba41e
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-04-11 20:32:49 +0200

    add SetUpFixture to install Playwright when running locally

---
+++ tests/web/Jordnaer.E2E.Tests/SetUpFixture.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace Jordnaer.E2E.Tests;
+
+[SetUpFixture]
+public class SetUpFixture
+{
+	[OneTimeSetUp]
+	public void GlobalSetup()
+	{
+		//// We set this environment variable to false in the CI pipeline,
+		//// so if it's null or true, run installation
+		//var installPlaywright = Environment.GetEnvironmentVariable("InstallPlaywright");
+		//if (string.IsNullOrEmpty(installPlaywright) ||
+		//	installPlaywright.Equals(bool.TrueString, StringComparison.InvariantCultureIgnoreCase))
+		//{
+		Program.Main(["install"]);
+		//}
+	}
+}
