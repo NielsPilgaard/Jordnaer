f763073972b7171e2f9c2be3814ddfb120601176
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-04-11 22:16:03 +0200

    Update LoginTests.cs

--- tests/web/Jordnaer.E2E.Tests/LoginTests.cs
+++ tests/web/Jordnaer.E2E.Tests/LoginTests.cs
@@ -11,8 +11,7 @@ public class LoginTests : BrowserTest
 	[Test]
 	public void AuthenticationFlow()
 	{
-		// TODO: Run "powershell.exe .\playwright.ps1 codegen jordnaer.azurewebsites.net --save-storage=auth.json"
-
-		// TODO: Subsequent tests should use "powershell.exe .\playwright.ps1 codegen --load-storage=auth.json jordnaer.azurewebsites.net
+		// TODO: Run "powershell.exe .\playwright.ps1 codegen https://mini-moeder.dk --save-storage=auth.json"
+		// TODO: Subsequent tests should use "powershell.exe .\playwright.ps1 codegen --load-storage=auth.json https://mini-moeder.dk
 	}
 }
