6678559404235525a5beb9aeda9ac4ba88f633b4
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-04-27 22:37:46 +0200

    remove azure app config in tests, simplifying things quite a bit

--- src/container_apps/Jordnaer.Chat/Extensions/WebApplicationBuilderExtensions.cs
+++ src/container_apps/Jordnaer.Chat/Extensions/WebApplicationBuilderExtensions.cs
@@ -4,36 +4,34 @@ namespace Jordnaer.Chat;
 
 public static class WebApplicationBuilderExtensions
 {
-    public static WebApplicationBuilder AddMassTransit(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddMassTransit(config =>
-        {
-            config.UsingAzureServiceBus((context, azureServiceBus) =>
-            {
-                azureServiceBus.Host(builder.Configuration.GetConnectionString("AzureServiceBus"));
+	public static WebApplicationBuilder AddMassTransit(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddMassTransit(config =>
+		{
+			config.UsingAzureServiceBus((context, azureServiceBus) =>
+			{
+				azureServiceBus.Host(builder.Configuration.GetConnectionString("AzureServiceBus"));
 
-                azureServiceBus.ConfigureEndpoints(context);
-            });
-        });
+				azureServiceBus.ConfigureEndpoints(context);
+			});
+		});
 
-        return builder;
-    }
+		return builder;
+	}
 
-    public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
-    {
-        string dbConnectionString = GetConnectionString(builder.Configuration);
+	public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
+	{
+		var connectionString = GetConnectionString(builder.Configuration);
 
-        builder.Services.AddSqlServer<JordnaerDbContext>(dbConnectionString);
+		builder.Services.AddSqlServer<JordnaerDbContext>(connectionString);
 
-        builder.Services.AddHealthChecks().AddSqlServer(dbConnectionString);
+		builder.Services.AddHealthChecks().AddSqlServer(connectionString);
 
-        return builder;
-    }
+		return builder;
+	}
 
-    private static string GetConnectionString(IConfiguration configuration) =>
-        Environment.GetEnvironmentVariable("ConnectionStrings_JordnaerDbContext")
-        ?? Environment.GetEnvironmentVariable("ConnectionStrings__JordnaerDbContext")
-        ?? configuration.GetConnectionString("JordnaerDbContext")
-        ?? throw new InvalidOperationException(
-            $"Connection string 'JordnaerDbContext' not found.");
+	private static string GetConnectionString(IConfiguration configuration) =>
+		configuration.GetConnectionString(nameof(JordnaerDbContext))
+		?? throw new InvalidOperationException(
+			$"Connection string '{nameof(JordnaerDbContext)}' not found.");
 }
--- tests/web/Jordnaer.Tests/Infrastructure/JordnaerWebApplicationFactory.cs
+++ tests/web/Jordnaer.Tests/Infrastructure/JordnaerWebApplicationFactory.cs
@@ -2,13 +2,9 @@ using Jordnaer.Database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.FeatureManagement;
 using Testcontainers.MsSql;
 using Xunit;
 
@@ -16,42 +12,25 @@ namespace Jordnaer.Tests.Infrastructure;
 
 public class JordnaerWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-	public readonly MsSqlContainer Container = new MsSqlBuilder()
+	public readonly MsSqlContainer MsSqlContainer = new MsSqlBuilder()
 		.WithName($"SqlServerTestcontainer-{Guid.NewGuid()}")
 		.Build();
 
-	public async Task InitializeAsync() => await Container.StartAsync();
+	public async Task InitializeAsync() => await MsSqlContainer.StartAsync();
 
 	public new async Task DisposeAsync()
 	{
-		await Container.DisposeAsync();
+		await MsSqlContainer.DisposeAsync();
 		await base.DisposeAsync();
 	}
 
 	protected override void ConfigureWebHost(IWebHostBuilder builder)
 	{
-		builder.ConfigureAppConfiguration(configurationBuilder =>
-		{
-			var configuration = configurationBuilder.Build();
-
-			var connectionString = configuration.GetConnectionString("AppConfig");
-
-			configurationBuilder.AddAzureAppConfiguration(options => options.
-																	 Connect(connectionString)
-																	 .Select("*"));
-		});
+		builder.UseSetting($"ConnectionStrings:{nameof(JordnaerDbContext)}", MsSqlContainer.GetConnectionString());
 
 		builder.ConfigureTestServices(services =>
 		{
-			services.AddFeatureManagement();
-			services.AddAzureAppConfiguration();
-
 			services.RemoveAll<IHostedService>();
-
-			services.RemoveAll<JordnaerDbContext>();
-			services.RemoveAll<DbContextOptions<JordnaerDbContext>>();
-
-			services.AddSqlServer<JordnaerDbContext>(Container.GetConnectionString());
 		});
 
 		builder.ConfigureLogging(loggingBuilder => loggingBuilder.ClearProviders());
