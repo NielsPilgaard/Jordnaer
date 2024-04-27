5e1e6fe640e6c9e7375f0e392f58e453fc04fb05
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-04-27 22:09:55 +0200

    disable azure app config in dev

--- src/web/Jordnaer/Extensions/AzureAppConfigurationExtensions.cs
+++ src/web/Jordnaer/Extensions/AzureAppConfigurationExtensions.cs
@@ -6,7 +6,12 @@ public static class AzureAppConfigurationExtensions
 {
 	public static WebApplicationBuilder AddAzureAppConfiguration(this WebApplicationBuilder builder)
 	{
-		string? connectionString = builder.Configuration.GetConnectionString("AppConfig");
+		if (builder.Environment.IsDevelopment())
+		{
+			return builder;
+		}
+
+		var connectionString = builder.Configuration.GetConnectionString("AppConfig");
 		if (connectionString is null)
 		{
 			throw new InvalidOperationException("Failed to find connection string to Azure App Configuration. Keys checked: 'ConnectionStrings:AppConfig'");
@@ -18,13 +23,10 @@ public static class AzureAppConfigurationExtensions
 			options.Connect(connectionString)
 				// Load all keys that have no label
 				.Select("*")
-				// Configure to reload builder if the registered sentinel key is modified
 				.ConfigureRefresh(refreshOptions =>
-				{
-					refreshOptions.Register("Sentinel", refreshAll: true);
-					refreshOptions.SetCacheExpiration(TimeSpan.FromMinutes(5));
-				})
-				.UseFeatureFlags(flagOptions => flagOptions.CacheExpirationInterval = TimeSpan.FromMinutes(3)));
+					// Only reload configs if the 'Sentinel' key is modified
+					refreshOptions.Register("Sentinel", refreshAll: true))
+				.UseFeatureFlags());
 
 		return builder;
 	}
--- src/web/Jordnaer/Program.cs
+++ src/web/Jordnaer/Program.cs
@@ -98,6 +98,7 @@ else
 	app.UseExceptionHandler("/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
+	app.UseAzureAppConfiguration();
 }
 
 app.UseHttpsRedirection();
@@ -114,8 +115,6 @@ app.UseStaticFiles(new StaticFileOptions
 app.UseRouting();
 
 app.UseSerilog();
-
-app.UseAzureAppConfiguration();
 
 app.UseAuthentication();
 app.UseAuthorization();
