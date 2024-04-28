a27544e5650592c3023505690b2a6dd57a09f183
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-04-28 21:50:21 +0200

    remove dependency on azure blob storage in testing, testcontainers ftw

--- tests/web/Jordnaer.Tests/Infrastructure/JordnaerWebApplicationFactory.cs
+++ tests/web/Jordnaer.Tests/Infrastructure/JordnaerWebApplicationFactory.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Testcontainers.Azurite;
 using Testcontainers.MsSql;
 using Xunit;
 
@@ -12,21 +13,33 @@ namespace Jordnaer.Tests.Infrastructure;
 
 public class JordnaerWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-	public readonly MsSqlContainer MsSqlContainer = new MsSqlBuilder()
+	private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
 		.WithName($"SqlServerTestcontainer-{Guid.NewGuid()}")
 		.Build();
 
-	public async Task InitializeAsync() => await MsSqlContainer.StartAsync();
+	private readonly AzuriteContainer _azureBlobStorageContainer = new AzuriteBuilder()
+		.WithName($"AzuriteTestcontainer-{Guid.NewGuid()}")
+		.WithInMemoryPersistence()
+		.Build();
+
+	public async Task InitializeAsync()
+	{
+		await _msSqlContainer.StartAsync();
+		await _azureBlobStorageContainer.StartAsync();
+	}
 
 	public new async Task DisposeAsync()
 	{
-		await MsSqlContainer.DisposeAsync();
+		await _msSqlContainer.DisposeAsync();
+		await _azureBlobStorageContainer.DisposeAsync();
 		await base.DisposeAsync();
 	}
 
 	protected override void ConfigureWebHost(IWebHostBuilder builder)
 	{
-		builder.UseSetting($"ConnectionStrings:{nameof(JordnaerDbContext)}", MsSqlContainer.GetConnectionString());
+		builder.UseSetting($"ConnectionStrings:{nameof(JordnaerDbContext)}", _msSqlContainer.GetConnectionString());
+
+		builder.UseSetting("ConnectionStrings:AzureBlobStorage", _azureBlobStorageContainer.GetConnectionString());
 
 		builder.ConfigureTestServices(services => services.RemoveAll<IHostedService>());
 
