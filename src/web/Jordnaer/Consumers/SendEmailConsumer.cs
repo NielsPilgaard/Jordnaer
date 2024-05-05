9f02ef133215140147a6faeb7495ebc4d0aff0ed
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-05-05 20:28:32 +0200

    add retry

--- src/web/Jordnaer/Consumers/SendEmailConsumer.cs
+++ src/web/Jordnaer/Consumers/SendEmailConsumer.cs
@@ -1,8 +1,12 @@
 using Jordnaer.Extensions;
 using Jordnaer.Features.Email;
 using MassTransit;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using Response = SendGrid.Response;
 
 namespace Jordnaer.Consumers;
 
@@ -11,6 +15,28 @@ public class SendEmailConsumer(
 	ISendGridClient sendGridClient)
 	: IConsumer<SendEmail>
 {
+	private static readonly ResiliencePipeline<Response> Retry = new ResiliencePipelineBuilder<Response>()
+		.AddRetry(new RetryStrategyOptions<Response>
+		{
+			ShouldHandle = new PredicateBuilder<Response>()
+				.Handle<Exception>()
+				.HandleResult(x => !x.IsSuccessStatusCode),
+			BackoffType = DelayBackoffType.Exponential,
+			Delay = TimeSpan.FromSeconds(1),
+			UseJitter = true,
+			MaxDelay = TimeSpan.FromSeconds(30),
+			MaxRetryAttempts = 8,
+			Name = "SendGridRetry"
+		})
+		.AddCircuitBreaker(new CircuitBreakerStrategyOptions<Response>
+		{
+			FailureRatio = 0.20,
+			SamplingDuration = TimeSpan.FromSeconds(30),
+			MinimumThroughput = 5,
+			BreakDurationGenerator = static args => new ValueTask<TimeSpan>(TimeSpan.FromSeconds(args.FailureCount * 30)),
+		})
+		.Build();
+
 	private static readonly TrackingSettings DefaultTrackingSettings = new()
 	{
 		ClickTracking = new ClickTracking { Enable = false },
@@ -24,7 +50,6 @@ public class SendEmailConsumer(
 		logger.LogFunctionBegan();
 
 		var message = consumeContext.Message;
-
 		var email = new SendGridMessage
 		{
 			From = message.From ?? EmailConstants.ContactEmail, // Must be from a verified email
@@ -41,7 +66,10 @@ public class SendEmailConsumer(
 
 		email.TrackingSettings = message.TrackingSettings ?? DefaultTrackingSettings;
 
-		var response = await sendGridClient.SendEmailAsync(email, consumeContext.CancellationToken);
+		var response = await Retry.ExecuteAsync(
+						   async token => await sendGridClient.SendEmailAsync(email, token),
+						   consumeContext.CancellationToken);
+
 		if (!response.IsSuccessStatusCode)
 		{
 			logger.LogError("Failed to send email to {Recipient}. " +
