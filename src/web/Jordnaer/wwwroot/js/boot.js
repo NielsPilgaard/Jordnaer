813e00b6c86f34cfc099db07fcc5e918e9619bdb
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-05-05 20:11:31 +0200

    A lot more retries, don't show the user the connection is down

--- src/web/Jordnaer/wwwroot/js/boot.js
+++ src/web/Jordnaer/wwwroot/js/boot.js
@@ -1,17 +1,13 @@
 (() => {
-    const maximumRetryCount = 3;
+    const maximumRetryCount = 150;
     const retryIntervalMilliseconds = 5000;
-    const reconnectModal = document.getElementById('reconnect-modal');
 
     const startReconnectionProcess = () => {
-        reconnectModal.style.display = 'block';
-
         let isCanceled = false;
 
         (async () => {
             for (let i = 0; i < maximumRetryCount; i++) {
-                reconnectModal.innerText = `Attempting to reconnect: ${i + 1} of ${maximumRetryCount}`;
-
+                console.log(`Attempting to reconnect: Attempt ${i + 1} of ${maximumRetryCount}`);
                 await new Promise(resolve => setTimeout(resolve, retryIntervalMilliseconds));
 
                 if (isCanceled) {
@@ -28,8 +24,8 @@
 
                     // Successfully reconnected to the server.
                     return;
-                } catch {
-                    // Didn't reach the server; try again.
+                } catch (exception) {
+                    console.log('Failed to reach the server, trying again.');
                 }
             }
 
@@ -40,7 +36,6 @@
         return {
             cancel: () => {
                 isCanceled = true;
-                reconnectModal.style.display = 'none';
             }
         };
     };
@@ -48,11 +43,13 @@
     let currentReconnectionProcess = null;
 
     Blazor.start({
-        reconnectionHandler: {
-            onConnectionDown: () => currentReconnectionProcess ??= startReconnectionProcess(),
-            onConnectionUp: () => {
-                currentReconnectionProcess?.cancel();
-                currentReconnectionProcess = null;
+        circuit: {
+            reconnectionHandler: {
+                onConnectionDown: () => currentReconnectionProcess ??= startReconnectionProcess(),
+                onConnectionUp: () => {
+                    currentReconnectionProcess?.cancel();
+                    currentReconnectionProcess = null;
+                }
             }
         }
     });
