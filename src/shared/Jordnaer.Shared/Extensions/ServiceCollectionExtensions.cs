473b0328abc8bd9e0880b39b98840d191d659ddc
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-05-13 21:25:57 +0200

    make data forsyningen ping more forgiving

--- src/shared/Jordnaer.Shared/Extensions/ServiceCollectionExtensions.cs
+++ src/shared/Jordnaer.Shared/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http.Resilience;
 using Microsoft.Extensions.Options;
+using Polly;
 using Refit;
 
 namespace Jordnaer.Shared;
@@ -21,6 +23,29 @@ public static class ServiceCollectionExtensions
 					client.BaseAddress = new Uri(options.BaseUrl);
 				})
 				.AddStandardResilienceHandler();
+
+		var pingCircuitBreakerOptions = new HttpCircuitBreakerStrategyOptions
+		{
+			FailureRatio = 0.5,
+			BreakDuration = TimeSpan.FromMinutes(5),
+			MinimumThroughput = 2,
+			ShouldHandle = arguments =>
+				new ValueTask<bool>(
+					arguments.Outcome.Exception is not null ||
+					arguments.Outcome.Result?.IsSuccessStatusCode is false)
+		};
+		services.AddRefitClient<IDataForsyningenPingClient>()
+				.ConfigureHttpClient((provider, client) =>
+				{
+					var options = provider.GetRequiredService<IOptions<DataForsyningenOptions>>().Value;
+
+					client.BaseAddress = new Uri(options.BaseUrl);
+				})
+				// This resilience strategy will pause all pings for 5 minutes if the API returns an error.
+				.AddResilienceHandler("ping",
+									  builder => builder.AddCircuitBreaker(pingCircuitBreakerOptions)
+														.AddConcurrencyLimiter(1)
+														.AddTimeout(TimeSpan.FromSeconds(30)));
 
 		return services;
 	}
