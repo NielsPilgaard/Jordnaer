2fda91ee1ec3038d0571f089cb4d03e5eb8ed221
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-06-26 22:26:05 +0200

    formatting

--- src/web/Jordnaer/Features/Profile/BrowserFileValidator.cs
+++ src/web/Jordnaer/Features/Profile/BrowserFileValidator.cs
@@ -6,14 +6,14 @@ namespace Jordnaer.Features.Profile;
 public class BrowserFileValidator : AbstractValidator<IBrowserFile>
 {
 	public const int MaxFileSize = 2_097_152;
-	private static readonly string[] _validContentTypes = { "image/jpeg", "image/png" };
+	private static readonly string[] ValidContentTypes = ["image/jpeg", "image/png"];
 	public BrowserFileValidator()
 	{
 		// Stop validation if this fails
 		RuleFor(file => file).Cascade(CascadeMode.Stop).NotEmpty();
 
 		RuleFor(x => x!.ContentType)
-			.Must(contentType => _validContentTypes.Contains(contentType))
+			.Must(contentType => ValidContentTypes.Contains(contentType))
 			.WithMessage("Kun JPEG og PNG billeder godtages.");
 
 		// 2 (MB) * 1024 (bytes -> kilobytes) * 1024 (kilobytes -> megabytes)
