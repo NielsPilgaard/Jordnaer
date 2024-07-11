a000e9672e8ec8675510bee294cf1dcc84a49e39
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-07-11 22:38:49 +0200

    add bunit test project

---
+++ tests/web/Jordnaer.bUnit.Tests/PageTitleTests.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using FluentAssertions;
+using Microsoft.AspNetCore.Components;
+
+namespace Jordnaer.Tests;
+
+public class PageTitleTests : TestContext
+{
+	[Fact]
+	public void AllPages_SetTitle()
+	{
+		// Arrange
+		var routableComponents = GetRoutableComponents();
+		foreach (var componentType in routableComponents)
+		{
+			// Act
+			var component = RenderComponent<UserSearch>();
+
+			// Assert
+			component.HasComponent<MetadataComponent>().Should().BeTrue();
+		}
+	}
+
+	public IEnumerable<Type> GetRoutableComponents()
+	{
+		// Assuming your components are in the same assembly as the entry point
+		var assembly = typeof(Program).Assembly;
+		var routableComponents = assembly.GetTypes()
+										 .Where(t => t.GetCustomAttributes<RouteAttribute>(inherit: false).Any());
+
+		return routableComponents;
+	}
+}
