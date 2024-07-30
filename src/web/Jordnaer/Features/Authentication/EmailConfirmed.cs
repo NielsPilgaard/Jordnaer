0e8dd13d0107993bd30c36e6dfbff1badd5e599f
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-07-30 15:08:48 +0200

    Create EmailConfirmed.cs

---
+++ src/web/Jordnaer/Features/Authentication/EmailConfirmed.cs
@@ -0,0 +1,16 @@
+﻿namespace Jordnaer.Features.Authentication;
+
+public class ConfirmEmailStateContainer
+{
+	public string? UserId { get; private set; }
+
+	public event Action? OnEmailConfirmed;
+
+	public void EmailConfirmed(string userId)
+	{
+		UserId = userId;
+		NotifyStateChanged();
+	}
+
+	private void NotifyStateChanged() => OnEmailConfirmed?.Invoke();
+}
