4caa818a0e54a300d7b0f260aff2d98f7a255a93
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-08-11 23:07:01 +0200

    add provider to external login metric

--- src/web/Jordnaer/Features/Metrics/JordnaerMetrics.cs
+++ src/web/Jordnaer/Features/Metrics/JordnaerMetrics.cs
@@ -10,10 +10,10 @@ internal static class JordnaerMetrics
 		new(name: AssemblyName.Name!,
 			version: AssemblyName.Version?.ToString());
 
-	internal static readonly Counter<int> ChatMessagesReceivedCounter =
-		Meter.CreateCounter<int>("jordnaer_chat_messages_received_total");
 	internal static readonly Counter<int> ChatMessagesSentCounter =
 		Meter.CreateCounter<int>("jordnaer_chat_messages_sent_total");
+	internal static readonly Counter<int> ChatMessagesReceivedCounter =
+		Meter.CreateCounter<int>("jordnaer_chat_messages_received_total");
 	internal static readonly Counter<int> ChatStartedSentCounter =
 		Meter.CreateCounter<int>("jordnaer_chat_chat_started_sent_total");
 	internal static readonly Counter<int> ChatStartedReceivedCounter =
