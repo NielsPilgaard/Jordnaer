8a4030290ac69face349058a58c40fc4e87f1460
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-09-14 20:50:31 +0200

    register post services

---
+++ src/web/Jordnaer/Features/PostSearch/WebApplicationExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Jordnaer.Features.PostSearch;
+
+public static class WebApplicationExtensions
+{
+	public static WebApplicationBuilder AddPostSearchService(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddScoped<IPostSearchService, PostSearchService>();
+
+		return builder;
+	}
+}
---
+++ src/web/Jordnaer/Features/Posts/WebApplicationExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Jordnaer.Features.Posts;
+
+public static class WebApplicationExtensions
+{
+	public static WebApplicationBuilder AddPostService(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddScoped<IPostService, PostService>();
+
+		return builder;
+	}
+}
