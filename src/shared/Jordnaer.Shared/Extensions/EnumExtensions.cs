ab67a364ab9ee0180dd189e23ebfe2ea7c384121
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2025-01-02 22:48:39 +0100

    minor cleanup and bug fixes
    I ended up scrapping categories as enums, it's not worth it

---
+++ src/shared/Jordnaer.Shared/Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jordnaer.Shared.Extensions;
+public static class EnumExtensions
+{
+	public static DisplayAttribute? GetDisplayAttribute(this Enum enumValue)
+	{
+		return enumValue.GetType()
+						.GetField(enumValue.ToString())?.GetCustomAttribute<DisplayAttribute>();
+	}
+	
+	public static string GetDisplayName(this Enum enumValue)
+	{
+		return enumValue.GetDisplayAttribute()?.Name ?? enumValue.ToString();
+	}
+}
