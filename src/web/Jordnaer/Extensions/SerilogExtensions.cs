a14292d8dec32cb6308baa3dc4880a16f3a5d918
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2025-03-25 08:35:14 +0100

    clear unused usings (merge misshap)

--- src/web/Jordnaer/Extensions/SerilogExtensions.cs
+++ src/web/Jordnaer/Extensions/SerilogExtensions.cs
@@ -1,8 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Serilog;
 using Serilog.Events;
