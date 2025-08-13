09ce19472906678b4e7b16cd882fb705128345f3
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2025-08-13 08:28:02 +0200

    Create EnumExtensions.cs

---
+++ src/web/Jordnaer/Extensions/EnumExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Jordnaer.Extensions;
+
+public static class EnumExtensions
+{
+	public static string? ToDisplayName<T>(this T enumValue) where T : Enum =>
+		enumValue.GetType()
+				 .GetMember(enumValue.ToString())
+				 .First()
+				 .GetCustomAttribute<DisplayAttribute>()?
+				 .Name;
+}
