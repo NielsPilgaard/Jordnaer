40719f2f0d773cc089422c962ccacc2a41f85f23
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2025-08-29 22:52:16 +0200

    remove redundant enum extensions class

--- src/shared/Jordnaer.Shared/Extensions/EnumExtensions.cs
+++ src/shared/Jordnaer.Shared/Extensions/EnumExtensions.cs
@@ -7,11 +7,16 @@ public static class EnumExtensions
 	public static DisplayAttribute? GetDisplayAttribute(this Enum enumValue)
 	{
 		return enumValue.GetType()
-						.GetField(enumValue.ToString())?.GetCustomAttribute<DisplayAttribute>();
+						.GetField(enumValue.ToString())?
+						.GetCustomAttribute<DisplayAttribute>();
 	}
 
 	public static string GetDisplayName(this Enum enumValue)
 	{
 		return enumValue.GetDisplayAttribute()?.Name ?? enumValue.ToString();
 	}
+
+	public static string? ToDisplayName<T>(this T enumValue) where T : Enum =>
+		enumValue.GetDisplayAttribute()?
+			.GetName();
 }
