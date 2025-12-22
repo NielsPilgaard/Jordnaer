646a4260fc4cee0e9c9aec2497d903305cc01ed1
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2025-12-22 18:25:19 +0100

    add error boundary to suppress blazor server -> blazor static ssr transition exception

--- src/web/Jordnaer/Components/Account/IdentityUserAccessor.cs
+++ src/web/Jordnaer/Components/Account/IdentityUserAccessor.cs
@@ -5,13 +5,15 @@ namespace Jordnaer.Components.Account;
 
 internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
 {
-	public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+	public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext? context)
 	{
+		ArgumentNullException.ThrowIfNull(context);
+
 		var user = await userManager.GetUserAsync(context.User);
 
 		if (user is null)
 		{
-			redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Kunne ikke indlæse bruger med ID '{userManager.GetUserId(context.User)}'.", context);
+			redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Kunne ikke indlÃ¦se bruger med ID '{userManager.GetUserId(context.User)}'.", context);
 		}
 
 		return user;
