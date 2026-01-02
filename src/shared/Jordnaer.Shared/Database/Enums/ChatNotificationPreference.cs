2ea85685a9bf650282b556d80a02ad21732ca0b6
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2026-01-02 23:20:45 +0100

    Update ChatNotificationPreference.cs

--- src/shared/Jordnaer.Shared/Database/Enums/ChatNotificationPreference.cs
+++ src/shared/Jordnaer.Shared/Database/Enums/ChatNotificationPreference.cs
@@ -6,11 +6,11 @@ namespace Jordnaer.Shared;
 [EnumExtensions]
 public enum ChatNotificationPreference
 {
-	[Display(Name = "Ingen")]
-	None = 0,
-
 	[Display(Name = "Kun første besked i ny samtale")]
-	FirstMessageOnly = 1,
+	FirstMessageOnly = 0,
+
+	[Display(Name = "Ingen")]
+	None = 1,
 
 	[Display(Name = "Alle beskeder")]
 	AllMessages = 2
